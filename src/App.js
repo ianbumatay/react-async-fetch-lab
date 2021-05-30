f832b82a64a7a9265729fc73e2a8720521801d9b
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-05-30 16:03:02 -0400

    commit tree 04273b974164978af45a8972838ebe072693d971

--- src/App.js
+++ src/App.js
@@ -1,1 +1,39 @@
-// create your App component here
+// create your App component here 
+
+
+
+import React, {Component} from 'react' 
+
+class App extends Component {  
+
+
+    state = {
+        people: []
+    } 
+
+
+    render(){ 
+        console.log("people:", this.state.people) 
+        return(
+            <div>
+                {this.state.people.map( person =>  <ul>{person.name}</ul> )}
+            
+            </div>
+        )
+    } 
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(res => res.json()) 
+        .then(data => {
+            this.setState({
+                people: data.people
+            })
+        })
+    }
+
+} 
+
+export default App;
+
+
